b6a99ce847c00b59b0c6a26e6c6abc68f331a0ff
Declan Cross <crossdecl@ecs.vuw.ac.nz>
2023-07-24 11:29:37 +1200

    Initial merger of existing fractal codebase

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,23 +1,302 @@
+// LEAVE THIS UP HERE
+var darkMode = null;
+var fractalOptions;
+var gemOptions;
+
+
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+// =======================================================================
+darkMode = false;
+fractalOptions = christmas;
+gemOptions = christmasGems;
+
+//  ===== or add your own custom fractalOptions and gemOptions here ======
+// fractalOptions = {
+//     background: 88,                        // 0-100 for brightness or hexcode string
+//     colours: [55, 99, 36, 85, 70, 15],     // hue of gems from 0-100
+//     stroke: 150,                           // brightness of lines from 0-100
+//     strokeWeight: 2.5,                     // thickness of lines
+//     maxDepth: 3,                           // max recursion depth of fractal
+//     minPoints: 3,                          // min number of points in a gem
+//     maxPoints: 10,                         // max number of points in a gem
+//     shadowOffset: 3,                       // offset of shadow from gem
+//     allowInitialGem: true,                 // allow initial gem to be drawn at max size
+//   },
+// gemOptions = {
+//     shadowOpacity: 45,                     // opacity of shadow from 0-100
+//     shadowX: 30,                           // x offset of shadow
+//     shadowY: 15,                           // y offset of shadow
+//     shadowBlur: 40,                        // blur of shadow
+//     rotation: -Math.PI / 2,                // rotation of gems
+//     outlines: true,                        // whether to draw outlines on gems
+//   }
+
+// =======================================================================
+
+
+
+//setup - variables
+const canvasDimensions = {
+  width: 200,
+  height: 200
+}
+
+
+
+
+// default values
+if (darkMode === null) darkMode = Math.random() > 0.8 ? true : false;
+if (!fractalOptions) fractalOptions = darkMode ? darkOptions.fractalOptions : lightOptions.fractalOptions;
+if (!gemOptions) gemOptions = gemOptions = darkMode ? darkOptions.gemOptions : lightOptions.gemOptions;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.resolution(FIT_TO_SCREEN); // FIT_TO_SCREEN
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = 200;
+  pWallpaper.grid_settings.cell_width = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset = 100;
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  let body = document.body;
+  if (typeof fractalOptions.background == 'string') {
+    background(fractalOptions.background);
+    body.style.backgroundColor = fractalOptions.background;
+  } else {
+    background(fractalOptions.background * 2.55, fractalOptions.background * 2.55, fractalOptions.background * 2.55); //light honeydew green colour
+    body.style.backgroundColor = 'rgb(' + fractalOptions.background * 2.55 + ', ' + fractalOptions.background * 2.55 + ', ' + fractalOptions.background * 2.55 + ')';
+  }
 }
 
+
+
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  angleMode(RADIANS);
+  colorMode(HSB, 100);
+
+  // draws initial recursive segment
+  drawSegment(0, 0, canvasDimensions.width - fractalOptions.strokeWeight / 2, canvasDimensions.height - fractalOptions.strokeWeight / 2, 0);
+  noFill();
+
+  // draws outer shadow
+  strokeWeight(fractalOptions.strokeWeight);
+  stroke(0, 0, 0, 5);
+  rect(fractalOptions.shadowOffset, fractalOptions.shadowOffset, canvasDimensions.width, canvasDimensions.height);
+
+  // draws outer boundary
+  stroke(fractalOptions.stroke);
+  strokeWeight(fractalOptions.strokeWeight / 3);
+  rect(0, 0, canvasDimensions.width, canvasDimensions.height);
+}
+
+
+
+
+
+function drawSegment(x, y, width, height, depth, noRepeat) {
+  if (depth > fractalOptions.maxDepth || width < 7 || height < 7) {
+    return;
+  }
+
+  if (random(1) > 0.85 && depth <= 1 && fractalOptions.allowInitialGem) {
+    generateGem(x + width / 2, y + height / 2, width / 2, width / 4, Math.floor(random(fractalOptions.minPoints, fractalOptions.maxPoints)), gemOptions.rotation, fractalOptions.colours[Math.floor(random(0, fractalOptions.colours.length))]);
+    return;
+  }
+
+
+  // draw boundaries shadows ( + )
+  strokeWeight(fractalOptions.strokeWeight);
+  stroke(0, 0, 0, 5);
+  let shadowOffset = fractalOptions.shadowOffset;
+  line(shadowOffset + x + width / 2 + fractalOptions.strokeWeight / 4, shadowOffset + y, shadowOffset + x + width / 2 + fractalOptions.strokeWeight / 4, shadowOffset + y + height);
+  line(shadowOffset + x, shadowOffset + y + height / 2 + fractalOptions.strokeWeight / 4, shadowOffset + x + width, shadowOffset + y + height / 2 + fractalOptions.strokeWeight / 4);
+  // draw boundaries ( + )
+  strokeWeight(fractalOptions.strokeWeight / 2);
+  stroke(fractalOptions.stroke);
+  line(x + width / 2 + fractalOptions.strokeWeight / 4, y, x + width / 2 + fractalOptions.strokeWeight / 4, y + height);
+  line(x, y + height / 2 + fractalOptions.strokeWeight / 4, x + width, y + height / 2 + fractalOptions.strokeWeight / 4);
+
+  if (noRepeat) {
+    return;
+  }
+
+  let circleCount = 0;
+  // call recursion with all four corners
+  for (let row = 0; row < 2; row++) {
+    for (let col = 0; col < 2; col++) {
+      var recursionProb = 1;
+      if (depth != 0) {
+        recursionProb = random(1) > 0.35;
+      }
+
+      if (recursionProb) { //&& !(row == 1 && col == 1 && circleCount == 0)
+        const newX = x + col * width / 2;
+        const newY = y + row * height / 2;
+        const newWidth = width / 2;
+        const newHeight = height / 2;
+        // allow second chance of extra empty recursion at max depth
+        if (depth == fractalOptions.maxDepth && random(1) > 0.7) {
+          drawSegment(newX, newY, newWidth, newHeight, depth, true);
+          continue;
+        }
+        drawSegment(newX, newY, newWidth, newHeight, depth + 1);
+      } else {
+        circleCount++;
+        const newX = x + col * width / 2 + fractalOptions.strokeWeight / 2 + 1;
+        const newY = y + row * height / 2 + fractalOptions.strokeWeight / 2 + 1;
+        const newWidth = width / 2 - fractalOptions.strokeWeight / 2 - 2;
+        const newHeight = height / 2 - fractalOptions.strokeWeight / 2 - 2;
+        generateGem(newX + newWidth / 2, newY + newHeight / 2, newWidth / 2, newWidth / 4, Math.floor(random(fractalOptions.minPoints, fractalOptions.maxPoints)), gemOptions.rotation, fractalOptions.colours[Math.floor(random(0, fractalOptions.colours.length))]);
+      }
+    }
+  }
+}
+
+// creates a gem given the center, radius, inner radius, amount of sides, rotation and colour
+function generateGem(centerX, centerY, radius, innerRadius, sides, rotation, col) {
+  let on = 100;
+  if (darkMode) {
+    // random chance of being 15 or 100
+    on = random(1) > 0.69 ? 100 : 15;
+  }
+
+  if (!rotation) {
+    rotation = Math.random() * TWO_PI;
+  }
+  let angle = TWO_PI / sides;
+
+  // draw outer background and find vertical shift
+  fill(0);
+  if (gemOptions.outlines) {
+    stroke(col, 60, 80, on == 100 ? 100 : 3);
+    strokeWeight(1);
+  } else {
+    noStroke();
+  }
+  push();
+  shadow(color(col, 100, 50, gemOptions.shadowOpacity), gemOptions.shadowX, gemOptions.shadowY, gemOptions.shadowBlur, on);
+  let smallestY = Infinity;
+  let largestY = -Infinity;
+  let smallestX = Infinity;
+  let largestX = -Infinity;
+  let vertexes = [];
+
+  for (let a = rotation; a < TWO_PI + rotation; a += angle) {
+    let innerX = centerX + cos(a) * radius;
+    let innerY = centerY + sin(a) * radius;
+    if (innerY < smallestY) {
+      smallestY = innerY;
+    }
+    if (innerY > largestY) {
+      largestY = innerY;
+    }
+    if (innerX < smallestX) {
+      smallestX = innerX;
+    }
+    if (innerX > largestX) {
+      largestX = innerX;
+    }
+    vertexes.push([innerX, innerY]);
+  }
+  // calculate vertical shift
+  let verticalShift = -1 * ((largestY - centerY) - (centerY - smallestY)) / 2;
+  let horizontalShift = -1 * ((largestX - centerX) - (centerX - smallestX)) / 2;
+
+  // draw shadow
+  beginShape();
+  for (let i = 0; i < vertexes.length; i++) {
+    vertex(vertexes[i][0] + horizontalShift, vertexes[i][1] + verticalShift);
+  }
+  endShape(CLOSE);
+  pop();
+
+  push();
+  // draw gem
+  for (let a = rotation; a <= TWO_PI + rotation - 0.00000001; a += angle) {
+    let outerX = centerX + cos(a) * radius + horizontalShift;
+    let outerY = centerY + sin(a) * radius + verticalShift;
+    let outerX2 = centerX + cos(a + angle) * radius + horizontalShift;
+    let outerY2 = centerY + sin(a + angle) * radius + verticalShift;
+
+    let innerX = centerX + cos(a) * innerRadius + horizontalShift;
+    let innerY = centerY + sin(a) * innerRadius + verticalShift;
+    let innerX2 = centerX + cos(a + angle) * innerRadius + horizontalShift;
+    let innerY2 = centerY + sin(a + angle) * innerRadius + verticalShift;
+
+    // find distance
+    let brightness = 100 - calculateAngleDistance(a + angle / 2, radians(240)) * (100 / Math.PI);
+    fill(0);
+    linearGradient(
+      (outerX + outerX2) / 2, // start x
+      (outerY + outerY2) / 2, // start y
+      (innerX + innerX2) / 2, // end x
+      (innerY + innerY2) / 2, // end y
+      color(col, 80, (brightness + 50) / 2, on), // outer colour (brightness averaged with default colour)
+      color(col + (100 - brightness) / 40, 80 - brightness / 3, brightness + 25, on), // inner colour (shading)
+    );
+
+    beginShape();
+    vertex(outerX, outerY);
+    vertex(outerX2, outerY2);
+    vertex(innerX2, innerY2);
+    vertex(innerX, innerY);
+    endShape(CLOSE);
+  }
+
+  // draw inner shape
+  fill(0);
+  linearGradient(
+    centerX + cos(radians(240)) * innerRadius, // start x
+    centerY + sin(radians(240)) * innerRadius, // start y
+    centerX + cos(PI + radians(240)) * innerRadius, // end x
+    centerY + sin(PI + radians(240)) * innerRadius, // end y
+    color(col, 65, 100, on), //Start color
+    color(col + 2.5, 90, 30, on), //End color
+  );
+  beginShape();
+  for (let a = rotation; a < TWO_PI + rotation; a += angle) {
+    let innerX = centerX + cos(a) * innerRadius + horizontalShift;
+    let innerY = centerY + sin(a) * innerRadius + verticalShift;
+    vertex(innerX, innerY);
+  }
+  endShape(CLOSE);
+  pop();
+}
+
+// Calculates the distrance between two angles including wrapping
+function calculateAngleDistance(angle1, angle2) {
+  let distance = Math.abs(angle1 - angle2);
+  if (distance > Math.PI) {
+    distance = 2 * Math.PI - distance;
+  }
+  return distance;
+}
+
+// draws a linear gradient given starting and ending X and Y coordinates and colours
+// uses javascript's drawing context rather than p5 as it is much more efficient
+function linearGradient(sX, sY, eX, eY, colorS, colorE) {
+  let gradient = drawingContext.createLinearGradient(
+    sX, sY, eX, eY
+  );
+  gradient.addColorStop(0, colorS);
+  gradient.addColorStop(1, colorE);
+  drawingContext.fillStyle = gradient;
+}
+
+// draws a shadow given colour, x and y offset, blur and whether the gem is on or off
+// uses javascript's drawing context rather than p5 as it is much more efficient
+function shadow(colour, x, y, blur, gemOn) {
+  drawingContext.shadowBlur = blur;
+  if (darkMode && gemOn != 100) {
+    return;
+  } else if (gemOn == 100) {
+    drawingContext.shadowBlur = blur * 2;
+  }
+  drawingContext.shadowColor = colour;
+  drawingContext.shadowOffsetX = x;
+  drawingContext.shadowOffsetY = y;
 }
---
+++ profiles.js
@@ -0,0 +1,152 @@
+// ========================================
+// DEFAULT OPTIONS
+// ========================================
+const lightOptions = {
+    fractalOptions: {
+        background: 88,
+        colours: [55, 99, 36, 85, 70, 15],
+        stroke: 150,
+        strokeWeight: 2.5,
+        maxDepth: 3,
+        minPoints: 3,
+        maxPoints: 10,
+        shadowOffset: 3,
+        allowInitialGem: true,
+    },
+    gemOptions: {
+        shadowOpacity: 45,
+        shadowX: 30,
+        shadowY: 15,
+        shadowBlur: 40,
+        rotation: -Math.PI / 2,
+        outlines: true,
+    }
+}
+
+const darkOptions = {
+    fractalOptions: {
+        background: 5,
+        colours: [55, 99, 36, 85, 70, 15],
+        stroke: 15,
+        strokeWeight: 2.5,
+        maxDepth: 3,
+        minPoints: 3,
+        maxPoints: 10,
+        shadowOffset: 3,
+        allowInitialGem: true,
+    },
+    gemOptions: {
+        shadowOpacity: 45,
+        shadowX: 0,
+        shadowY: 0,
+        shadowBlur: 120,
+        rotation: -Math.PI / 2,
+        outlines: true,
+    }
+}
+
+
+// ========================================
+// THEME OPTIONS
+// ========================================
+const desert = {
+    background: '#ba6657',
+    colours: [100, 95, 10, 5],
+    stroke: '#e6998c',
+    strokeWeight: 2.5,
+    maxDepth: 3,
+    minPoints: 3,
+    maxPoints: 10,
+    shadowOffset: 3,
+    allowInitialGem: true,
+}
+
+const atlantis = {
+    background: '#0e1a40',
+    colours: [50, 55, 60, 65, 70, 75],
+    stroke: '#325670',
+    strokeWeight: 2.5,
+    maxDepth: 2,
+    minPoints: 7,
+    maxPoints: 21,
+    shadowOffset: 3,
+    allowInitialGem: true,
+}
+
+const forest = {
+    background: '#293b25',
+    colours: [35, 40, 45],
+    stroke: '#86b37b',
+    strokeWeight: 2.5,
+    maxDepth: 1,
+    minPoints: 3,
+    maxPoints: 5,
+    shadowOffset: 3,
+    allowInitialGem: true,
+}
+
+const pixels = {
+    background: '#000000',
+    colours: [0, 33, 66],
+    stroke: '#222',
+    strokeWeight: 2.5,
+    maxDepth: 8,
+    minPoints: 4,
+    maxPoints: 4,
+    shadowOffset: 3,
+    allowInitialGem: true,
+}
+
+const christmas = {
+    background: '#fff',
+    colours: [0, 33],
+    stroke: '#ddd',
+    strokeWeight: 1,
+    maxDepth: 6,
+    minPoints: 35,
+    maxPoints: 45,
+    shadowOffset: 1,
+    allowInitialGem: true,
+}
+
+const cottonCandy = {
+    background: '#fff',
+    colours: [55, 89],
+    stroke: '#ddd',
+    strokeWeight: 2.5,
+    maxDepth: 3,
+    minPoints: 5,
+    maxPoints: 12,
+    shadowOffset: 3,
+    allowInitialGem: true,
+}
+
+
+// ========================================
+// GEM OPTIONS
+// ========================================
+const spinningGems = {
+    shadowOpacity: 45,
+    shadowX: 30,
+    shadowY: 15,
+    shadowBlur: 40,
+    outlines: true,
+}
+
+const pixelGems = {
+    shadowOpacity: 45,
+    shadowX: 30,
+    shadowY: 15,
+    shadowBlur: 40,
+    rotation: Math.PI / 4,
+    outlines: true,
+}
+
+const christmasGems = {
+    shadowOpacity: 25,
+    shadowX: 5,
+    shadowY: 5,
+    shadowBlur: 20,
+    rotation: -Math.PI / 2,
+    outlines: false,
+}
